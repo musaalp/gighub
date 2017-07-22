246cc1a7ecf724cdb2ca5182dcec17c3797141c6
Musa Alp <musaa.alp@gmail.com>
2017-07-23 00:24:29 +0300

    Add attendance controller

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -10,12 +10,23 @@ namespace GigHub.Models
         {
         }
 
+        public DbSet<Attendance> Attendances { get; set; }
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
 
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder); 
         }
     }
 }
