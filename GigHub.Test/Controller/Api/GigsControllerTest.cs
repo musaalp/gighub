c799a9b2dced965d2ddf4d8bccd3adb4941504f0
Musa Alp <musaa.alp@gmail.com>
2017-09-27 21:26:19 +0300

    Mocking the current user,

---
+++ GigHub.Test/Controller/Api/GigsControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using GigHub.Controllers.Api;
+using GigHub.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace GigHub.Test.Controller.Api
+{
+    [TestClass]
+    public class GigsControllerTest
+    {
+        private readonly GigsController _controller;
+
+        public GigsControllerTest()
+        {
+            var identity = new GenericIdentity("user1@domain.com");
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", "user1@domain.com"));
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "1"));
+
+            var principal = new GenericPrincipal(identity, null);
+
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            //mockUnitOfWork.Object means implemantation of IUnitOfwork
+            _controller = new GigsController(mockUnitOfWork.Object);
+            _controller.User = principal;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
